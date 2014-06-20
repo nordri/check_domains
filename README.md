d5eb8db5b00f67d4e1d0a050c06e87fedf28e7cf
nordri <nordri@gmail.com>
2014-06-20 18:04:52 +0200

    Settings

---
+++ README.md
@@ -0,0 +1,1 @@
+A little DJango App to check expiration dates on domains
--- domains/domains/settings.py
+++ domains/domains/settings.py
@@ -36,6 +36,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'check_domains',
 )
 
 MIDDLEWARE_CLASSES = (
@@ -66,9 +67,9 @@ DATABASES = {
 # Internationalization
 # https://docs.djangoproject.com/en/dev/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'es-es'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Europe/Madrid'
 
 USE_I18N = True
 
