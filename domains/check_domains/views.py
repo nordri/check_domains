f35d39f9a8a7874fd01cd425b78e5d31c8fb780c
nordri <nordri@gmail.com>
2014-06-20 18:26:41 +0200

    Views

--- domains/check_domains/views.py
+++ domains/check_domains/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from check_domains.models import Domain
+from datetime import datetime
+from django.shortcuts import render_to_response
+from django.template import RequestContext
 
 # Create your views here.
+def daysRemaining(d0, d1):
+  return d0 - d1
+
+def view_domain(request):
+  domainList = []
+  domains = Domain.objects.all().order_by('days')
+  for domain in domains:
+    daysLeft = daysRemaining (domain.days, datetime.today())
+    d = (domain.name, daysLeft)
+    domainList.append(d)
+    
+  return render_to_response('domains/view_domains.html',{ 'domainList':domainsList }, context_instance=RequestContext(request))
+  
+
