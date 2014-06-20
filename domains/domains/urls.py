3afaaa6ca979580399a4447545b675fd065cb4a7
nordri <nordri@gmail.com>
2014-06-20 18:22:04 +0200

    URLs

--- domains/domains/urls.py
+++ domains/domains/urls.py
@@ -2,9 +2,8 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'domains.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
+    # View -> Domains
+    url(r'^$', 'check_domains.views.view_domain', name='view_domain'),
 
     url(r'^admin/', include(admin.site.urls)),
 )
