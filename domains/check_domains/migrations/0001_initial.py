442745315767afa8ae14a06a6633889da7f02cae
nordri <nordri@gmail.com>
2014-06-20 18:07:43 +0200

    Migrations

---
+++ domains/check_domains/migrations/0001_initial.py
@@ -0,0 +1,24 @@
+# encoding: utf8
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='Domain',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('name', models.CharField(max_length=100)),
+                ('days', models.DateField()),
+            ],
+            options={
+            },
+            bases=(models.Model,),
+        ),
+    ]
