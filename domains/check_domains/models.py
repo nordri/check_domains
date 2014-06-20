b8d9491530f33c3a014894184b5ee5f2a4451142
nordri <nordri@gmail.com>
2014-06-20 18:06:24 +0200

    Models

--- domains/check_domains/models.py
+++ domains/check_domains/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Domain(models.Model):
+  name = models.CharField(max_length=100)
+  days = models.DateField()
+
+  def __str__(self):
+    return self.name
+
+  def __unicode__(self):
+    return self.name
+
