b6cb9a75cbcbcafa91f342be2bbe835a649687b5
nordri <nordri@gmail.com>
2014-07-01 13:24:58 +0200

    URL parameters to filter expiration dates

--- domains/check_domains/views.py
+++ domains/check_domains/views.py
@@ -7,14 +7,12 @@ from django.template import RequestContext
 def daysRemaining(d0, d1):
   return d0 - d1
 
-def view_domain(request):
+def view_domain(request, days_left=0):
   domainList = []
   domains = Domain.objects.all().order_by('days')
   for domain in domains:
     daysLeft = daysRemaining (domain.days, datetime.today().date())
-    d = (domain.name, daysLeft)
+    d = (domain.name, daysLeft.days)
     domainList.append(d)
     
-  return render_to_response('domains/view_domains.html',{ 'domainList':domainList }, context_instance=RequestContext(request))
-  
-
+  return render_to_response('domains/view_domains.html',{ 'domainList':domainList, 'days_left':days_left }, context_instance=RequestContext(request))
--- domains/domains/urls.py
+++ domains/domains/urls.py
@@ -4,6 +4,8 @@ from django.contrib import admin
 urlpatterns = patterns('',
     # View -> Domains
     url(r'^$', 'check_domains.views.view_domain', name='view_domain'),
-
+    # ability to filter
+    url(r'^filter/(?P<days_left>\d+)/$', 'check_domains.views.view_domain'),
+    # admin interface
     url(r'^admin/', include(admin.site.urls)),
 )
